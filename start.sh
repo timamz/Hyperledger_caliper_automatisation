6272ce8cf96067ad839694184577b986c477a905
timamz <timophey.maz@gmail.com>
2024-04-05 12:37:32 +0000

    add start script

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ../fabric-samples/test-network
+./network.sh up createChannel -s leveldb
+./network.sh deployCC -ccn smallbank -ccp ../../caliper-benchmarks/src/fabric/scenario/smallbank/go -ccl go
+cd ../../caliper-benchmarks
+npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/fabric/test-network.yaml --caliper-benchconfig benchmarks/scenario/smallbank/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
+mv report.html ../automatization/
+cd ../fabric-samples/test-network
+./network.sh down
+cd ../../automatization/
