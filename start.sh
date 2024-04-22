dfacbce27c7a66d366562b101b86f575de35b83d
timamz <timophey.maz@gmail.com>
2024-04-22 17:51:10 +0000

    clear from reports

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd ../fabric-samples/test-network
-./network.sh up createChannel -s leveldb
+./network.sh up createChannel -s couchdb
 ./network.sh deployCC -ccn smallbank -ccp ../../caliper-benchmarks/src/fabric/scenario/smallbank/go -ccl go
 cd ../../caliper-benchmarks
 npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/fabric/test-network.yaml --caliper-benchconfig benchmarks/scenario/smallbank/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
-mv report.html ../automatization/
+mv report.html ../automatization/reports
 cd ../fabric-samples/test-network
 ./network.sh down
 cd ../../automatization/
