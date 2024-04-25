f7e546b110f0c049770ebacbc742490f7783baa1
timamz <timophey.maz@gmail.com>
2024-04-25 19:22:49 +0000

    add building and deleting images to starting scipt

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ../fabric
+make clean docker-clean peer-docker orderer-docker tools-docker docker-thirdparty docker native
 cd ../fabric-samples/test-network
 ./network.sh up createChannel -s couchdb
 ./network.sh deployCC -ccn smallbank -ccp ../../caliper-benchmarks/src/fabric/scenario/smallbank/go -ccl go
@@ -8,4 +10,5 @@ mv report.html ../automatization/reports
 cd ../fabric-samples/test-network
 ./network.sh down
 cd ../../automatization/
+docker rmi -f $(docker images -aq)
 exit
