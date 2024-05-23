cd8f74bb8a7c3be7bdb3bbdaf10c53e003f28619
Timofey <timophey.maz@gmail.com>
2024-05-24 01:16:15 +0300

    change everything

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-cd ../fabric
-make clean docker-clean peer-docker orderer-docker tools-docker docker-thirdparty docker native
 cd ../fabric-samples/test-network
-./network.sh up createChannel -s couchdb
-./network.sh deployCC -ccn smallbank -ccp ../../caliper-benchmarks/src/fabric/scenario/smallbank/go -ccl go
-cd ../../caliper-benchmarks
-npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/fabric/test-network.yaml --caliper-benchconfig benchmarks/scenario/smallbank/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
-mv report.html ../automatization/reports
+./network.sh up createChannel
+./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript -ccl javascript
+cd ../../caliper-workspace
+npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/networkConfig.yaml --caliper-benchconfig benchmarks/myAssetBenchmark.yaml --caliper-flow-only-test
+mv report.html ../automatisation/reports/
 cd ../fabric-samples/test-network
 ./network.sh down
 cd ../../automatization/
